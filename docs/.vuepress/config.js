916a948b2885baca35c50525114e0d98590d2e3b
xiong <xiong@xiongdeiMac.local>
2018-10-13 23:34:32 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: '熊大叔',
+  description: '熊大叔放学习笔记的地方',
+  themeConfig: {
+    // navbar: false,
+    nav: [
+      // { text: '首页', link: '/' },
+      // { text: 'Guide', link: '/guide/' },
+      // { text: 'External', link: 'https://google.com' },
+    ],
+    lastUpdated: '最后更新时间',
+    // sidebar: 'auto'
+    sidebar: [
+      {
+        title: 'Unix哲学读书笔记',
+        children: [
+          '/Unix/PhilOverview',
+          '/Unix/PhilModel'
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/Unix/README.md
@@ -0,0 +1,6 @@
+# 《Unix编程艺术》读书笔记
+
+> 将读书的过程记录下来，既是留念，也是鞭策。
+
+* [Unix哲学之概论](/Unix/PhilOverview.md)
+* [Unix哲学之模块原则](/Unix/PhilModel.md)
